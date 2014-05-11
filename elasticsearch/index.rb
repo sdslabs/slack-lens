8049578a91c60fbe31ca6ed4708d7d4aa4ad5f75
Pravendra Singh <hackpravj@gmail.com>
2014-05-11 14:14:44 +0530

    shifts to seperate index for each channel

--- elasticsearch/index.rb
+++ elasticsearch/index.rb
@@ -3,6 +3,7 @@
 require 'json'
 
 require_relative 'client.rb'
+require_relative 'msg_id.rb'
 require_relative '../config/configer.rb'
 
 class Index
@@ -18,15 +19,17 @@ class Index
 		es = ES.new
 		client = es.client()
 
-		client.index 	index: @config.value('index'),
-			     				type: @config.value('type'),
-			     				body: {
-				   					user: @message['user'],
-				 						message: @message['message'],
-				 						channel: @message['channel'],
-				 						timestamp: @message['timestamp']
-			     				}
+		# different index for each channel
+		client.index 	index: @message['channel'],
+		type: 'Message',
+		body: {
+			user: @message['user'],
+			message: @message['message'],
+			channel: @message['channel'],
+			timestamp: @message['timestamp'],
+			msgid: MsgID.new(@message['channel']).assign()
+		}
 	end
 end
 
-Index.new(JSON.parse('{"user":"aps", "message":"whatever", "channel":"muzi", "timestamp": "564621"}')).index()
+Index.new(JSON.parse('{"user":"ravi", "message":"go kill yourself", "channel":"general", "timestamp": 564621}')).index()
--- elasticsearch/msg_id.rb
+++ elasticsearch/msg_id.rb
@@ -1,18 +1,13 @@
 # this script assigns/returns 'id' for a message according to 'channel'
-# it uses a char as a prefix to total message count for that channel
+# uses total message count for that channel
+# then each message is tracked by :channel+msgid
 
 require 'rest-client'
 require 'json'
 require_relative '../config/configer.rb'
 
-# hope nobody uses that much channels(>110) to break this
-$chars = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j"]
-$charlen = $chars.length
-
 
 class MsgID
-	# class var : list of all channels
-	@@channels = []
 
 	# set channel argument as instance var
 	def initialize(channel)
@@ -20,33 +15,20 @@ class MsgID
 		@config = Configer.new()
 	end
 
-	# returns prefix for a channel
-	def Prefix
-		# channel not there, in channel's list
-		if @@channels.index(@channel) == nil
-			# add channel now
-			@@channels.push(@channel)
-		end
-
-		if @@channels.length <= $charlen
-			prefix = $chars[@@channels.index(@channel)]
-			return prefix
-		else
-			prefix = $chars[(@@channels.length / $charlen)-1]
-			prefix = prefix + $chars[(@@channels.length % $charlen)-1]
-			return prefix
-		end
-	end
-
-	# assigns id, using prefix and total message count
+	# assigns id, using total message count
 	def assign
-		response = RestClient.get "http://localhost:9200/#{@config.value('index')}/#{@config.value('type')}/_count?q=channel:" + @channel
-		puts response
-		total = JSON.parse(response)['count']
-		puts Prefix()
-		id = Prefix() + "#{total+1}"
-		puts id
-	end
-end
+		# if index already exists for channel
+		begin
+			response = RestClient.get "#{@config.value('url')}/#{@channel}/#{@config.value('type')}/_count"
+			total = JSON.parse(response)['count']
+	    id = total+1
+	    return id
 
-MsgID.new('general').assign()
+	  # index doesn't exists, creates new
+		rescue
+			RestClient.put "#{@config.value('url')}/#{@channel}", :UserAgent => "slack-lens"
+			return 1
+		end
+	end
+
+end
