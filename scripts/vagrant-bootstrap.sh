297973b36ec8a457090f40b0afd41d6e5e26c3d3
Amanpreet Singh <apsdehal@gmail.com>
2015-08-19 14:50:31 +0530

    Update bootstrap script to include lein download

--- scripts/vagrant-bootstrap.sh
+++ scripts/vagrant-bootstrap.sh
@@ -4,7 +4,7 @@
 sudo add-apt-repository ppa:webupd8team/java
 sudo apt-get -y -q update
 echo oracle-java7-installer shared/accepted-oracle-license-v1-1 select true | sudo /usr/bin/debconf-set-selections
-sudo apt-get -y -q install oracle-java7-installer
+sudo apt-get -y -q install oracle-java7-installer rlwrap
 sudo apt-get install -y --force-yes oracle-java8-*
 sudo update-alternatives --set java /usr/lib/jvm/java-8-oracle/jre/bin/java
 
@@ -12,3 +12,9 @@ sudo update-alternatives --set java /usr/lib/jvm/java-8-oracle/jre/bin/java
 wget https://download.elasticsearch.org/elasticsearch/elasticsearch/elasticsearch-1.6.0.deb
 sudo dpkg -i elasticsearch-1.6.0.deb
 sudo service elasticsearch start # Run it
+
+curl -o- https://raw.githubusercontent.com/technomancy/leiningen/stable/bin/lein >> lein
+chmod +x lein 
+sudo mv lein /usr/local/bin/
+cd ~/dev/slack-lens
+lein deps
