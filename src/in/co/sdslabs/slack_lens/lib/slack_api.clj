baf8cdd6355a2fd9989e77e9461a47ad4577d68c
Amanpreet Singh <apsdehal@gmail.com>
2015-08-24 12:08:54 +0530

    Add helper for slack web api

---
+++ src/in/co/sdslabs/slack_lens/lib/slack_api.clj
@@ -0,0 +1,15 @@
+(ns in.co.sdslabs.slack-lens.lib.slack-api
+	(:require [clj-slack
+              [channels :as channels]]))
+
+(defn- join-channel
+  [{:keys [is_channel is_member id name]} options]
+  (when (and is_channel (not is_member))
+    (channels/join options name)))
+
+(defn join-all-channels
+  [options]
+  (let [response (channels/list options {:exclude_archived "1"})]
+    (if (:ok response)
+      (map #(join-channel % options) (:channels response))
+      (throw (ex-info "Failed to fetch channel api" {:operation "channel.list"})))))
