25e8c443a8c10a72cd23ec1f5ec07f73b1a215ec
harsh_jain <harshjniitr@gmail.com>
2017-10-09 21:08:20 +0530

    adding the test script

---
+++ src/in/co/sdslabs/slack_lens/test/message.clj
@@ -0,0 +1,22 @@
+(ns in.co.sdslabs.slack-lens.test.message
+  (:use [org.httpkit.server])
+  (:require [compojure.route :as route]
+          [cheshire.core :as json]))
+
+(def clients (atom {}))
+
+(defn ws
+  [req]
+  (with-channel req con
+    (swap! clients assoc con true)
+          (doseq [client @clients mes (clojure.string/split-lines (slurp "messages"))]
+            (Thread/sleep 2000)(send! (key client)  mes
+                   false) (prn mes))
+    (on-close con (fn [status]
+                    (swap! clients dissoc con)
+                    (println con " disconnected. status: " status)))))
+
+(defn -main [& args]
+  (let [port (Integer/parseInt 
+               (or (System/getenv "PORT") "8080"))]
+    (run-server ws {:port port :join? false})))
