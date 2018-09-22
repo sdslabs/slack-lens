67055046abcf55cb8bab71f35b9ef9f96e7713c8
Harshit Maurya <hmaurya999@gmail.com>
2018-09-23 04:39:28 +0530

    Updated elasticsearch version

--- scripts/vagrant-bootstrap.sh
+++ scripts/vagrant-bootstrap.sh
@@ -9,8 +9,8 @@ sudo apt-get install -y --force-yes oracle-java8-*
 sudo update-alternatives --set java /usr/lib/jvm/java-8-oracle/jre/bin/java
 
 # Install elasticsearch
-wget https://download.elasticsearch.org/elasticsearch/elasticsearch/elasticsearch-1.6.0.deb
-sudo dpkg -i elasticsearch-1.6.0.deb
+wget https://download.elasticsearch.org/elasticsearch/elasticsearch/elasticsearch-5.6.0.deb
+sudo dpkg -i elasticsearch-5.6.0.deb
 sudo service elasticsearch start # Run it
 
 curl -o- https://raw.githubusercontent.com/technomancy/leiningen/stable/bin/lein >> lein
